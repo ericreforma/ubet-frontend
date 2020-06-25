550ed6c0e1b40bdec14e029b1b5f546f04ad890e
ericreforma <ericreforma@gmail.com>
2020-06-26 01:43:24 +0800

    Updates

---
+++ precache-manifest.071260402a8e2ca5499f93fdc5c5dfab.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f562280e4fee7f7ec90d3cbbac9b4dd3",
+    "url": "/ubet-frontend/index.html"
+  },
+  {
+    "revision": "c68913acb7794e644482",
+    "url": "/ubet-frontend/static/css/main.95d2ba6a.chunk.css"
+  },
+  {
+    "revision": "22c369795d79fa9107c2",
+    "url": "/ubet-frontend/static/js/2.52e8671a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ubet-frontend/static/js/2.52e8671a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c68913acb7794e644482",
+    "url": "/ubet-frontend/static/js/main.51de7266.chunk.js"
+  },
+  {
+    "revision": "2bbcdc5d41b4b8214592",
+    "url": "/ubet-frontend/static/js/runtime-main.80da2468.js"
+  },
+  {
+    "revision": "2a78e9d43e7173a807df04eed4531109",
+    "url": "/ubet-frontend/static/media/banner.2a78e9d4.png"
+  },
+  {
+    "revision": "95388070e701e51cbc1424bc832d735e",
+    "url": "/ubet-frontend/static/media/customer-service.95388070.svg"
+  },
+  {
+    "revision": "f74756f0e973d109c8690c40b6a02843",
+    "url": "/ubet-frontend/static/media/customer-service_on.f74756f0.svg"
+  },
+  {
+    "revision": "e7d70bb4796fb8d336f7edb292d72b53",
+    "url": "/ubet-frontend/static/media/deposit.e7d70bb4.svg"
+  },
+  {
+    "revision": "a2d803f6c866d01462565e29dc4758b9",
+    "url": "/ubet-frontend/static/media/home.a2d803f6.svg"
+  },
+  {
+    "revision": "49713e17d21059574ae0ac615d64dec9",
+    "url": "/ubet-frontend/static/media/home_bg.49713e17.png"
+  },
+  {
+    "revision": "7540d4e856b6261c954275c3967694cb",
+    "url": "/ubet-frontend/static/media/home_on.7540d4e8.svg"
+  },
+  {
+    "revision": "6a1d45bdec9c9c21060fd865825cb46b",
+    "url": "/ubet-frontend/static/media/messages.6a1d45bd.svg"
+  },
+  {
+    "revision": "a8360631f094a1dbc83336cd9c40b6bd",
+    "url": "/ubet-frontend/static/media/nav-btn.a8360631.svg"
+  },
+  {
+    "revision": "9d5dbe971f0060f544e4bb38cd5620c7",
+    "url": "/ubet-frontend/static/media/profile.9d5dbe97.svg"
+  },
+  {
+    "revision": "6a2ba651de711ef3abefdec55a940d59",
+    "url": "/ubet-frontend/static/media/profile_on.6a2ba651.svg"
+  },
+  {
+    "revision": "53ded5d00e28e64dd0567c31d6978bb7",
+    "url": "/ubet-frontend/static/media/promotion.53ded5d0.svg"
+  },
+  {
+    "revision": "90f19d955508982f1a2601646ba791ca",
+    "url": "/ubet-frontend/static/media/promotion_on.90f19d95.svg"
+  },
+  {
+    "revision": "ca7ae086132016fb18cc7c59c817f8ac",
+    "url": "/ubet-frontend/static/media/transfer.ca7ae086.svg"
+  },
+  {
+    "revision": "8ebf856342518a80f12a49e81b5c28d0",
+    "url": "/ubet-frontend/static/media/withdrawal.8ebf8563.svg"
+  }
+]);
